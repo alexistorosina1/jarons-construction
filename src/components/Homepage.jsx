c969116b0515b6fc3cda56ee3d1fded1d8e9fd23
Alexis Torosina <alexistorosina1@gmail.com>
2026-06-02 15:53:39 -0400

    homepage intro section added

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,14 @@
+export default function Homepage() {
+  <>
+    <section>
+      <div>
+        <h1>Building Dreams Into Reality</h1>
+        <h2>
+          With over 15 years of experience, JARONS Construction delivers
+          <br /> exceptional quality and craftsmanship in every project.
+        </h2>
+        <button>Get a free Quote</button>
+      </div>
+    </section>
+  </>;
+}
