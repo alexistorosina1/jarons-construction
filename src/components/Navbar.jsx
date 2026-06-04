82718c74f21cd12343fdbc96e10d3fa7e310a709
Alexis Torosina <alexistorosina1@gmail.com>
2026-06-04 19:58:59 -0400

    got rid of navlink default css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,25 @@
+import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      <h1>JARONS</h1>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Services</li>
-        <li>Projects</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
+    <div>
+      <nav className="navbar">
+        <h1>JARONS</h1>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/services">Services</NavLink>
+          </li>
+          <li>
+            <NavLink to="/projects">Projects</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
