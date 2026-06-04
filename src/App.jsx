02f343122d488df0e2915be4d17328963514ad4f
Alexis Torosina <alexistorosina1@gmail.com>
2026-06-04 19:02:15 -0400

    installed browser route for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage.jsx";
 import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Homepage />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
