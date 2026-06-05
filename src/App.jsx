bc5c9f209dde0a87404f0e3e32903796192b7556
Alexis Torosina <alexistorosina1@gmail.com>
2026-06-05 15:55:06 -0400

    imported About component in the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage.jsx";
 import Navbar from "./components/Navbar.jsx";
+import About from "./components/About.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </Router>
     </>
