761f5306c43b4dcda9a5f3e3d24fc66e38d9da9d
Alexis Torosina <alexistorosina1@gmail.com>
2026-06-05 15:25:23 -0400

    added about link to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,6 +10,9 @@ export default function Navbar() {
             <NavLink to="/">Home</NavLink>
           </li>
           <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
             <NavLink to="/services">Services</NavLink>
           </li>
           <li>
